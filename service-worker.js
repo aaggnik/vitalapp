27db75716990d312055f3a688fb66650651f998a
aaggnik <juliusschumann13@gmail.com>
2025-09-24 17:33:09 +0200

    Add files via upload

---
+++ service-worker.js
@@ -0,0 +1,15 @@
+const CACHE_NAME = "vitalapp-cache-v1";
+const FILES_TO_CACHE = ["./","./index.html","./manifest.json","./icon.png"];
+
+self.addEventListener("install", (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then(c => c.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", (evt) => {
+  evt.respondWith(caches.match(evt.request).then(resp => resp || fetch(evt.request)));
+});
